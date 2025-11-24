56b0572292fe193201012f2e76e9fadcbb6005ea
Tracy Chan <67964235+tracy308@users.noreply.github.com>
2025-11-25 01:14:51 +0800

    Always search maps by location name

--- frontend/PeakFindr/PeakFindr/Services/Navigator.swift
+++ frontend/PeakFindr/PeakFindr/Services/Navigator.swift
@@ -3,8 +3,15 @@ import Foundation
 import UIKit
 
 struct Navigator {
-    static func openInMaps(urlString: String?) {
-        guard let urlString, let url = URL(string: urlString) else { return }
+    static func openInMaps(for location: LocationResponse) {
+        var queryParts: [String] = [location.name]
+        let region = location.area?.isEmpty == false ? location.area! : "Hong Kong"
+        queryParts.append(region)
+
+        let query = queryParts.joined(separator: ", ")
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+
+        guard let query, let url = URL(string: "http://maps.apple.com/?q=\(query)") else { return }
         UIApplication.shared.open(url)
     }
 }
